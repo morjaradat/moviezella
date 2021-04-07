0dca3c3a0747a4d0f387873f2485bf8c06960d54
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-04-07 11:21:58 +0300

    aboutAs

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -70,21 +70,21 @@ window.addEventListener('load', () => {
 });
 
 
-const ratingbox = document.getElementById('rating');
-const items = document.querySelectorAll('.rating-Item');
+// const ratingbox = document.getElementById('rating');
+// const items = document.querySelectorAll('.rating-Item');
 
 
 
-ratingbox.addEventListener('click', (e) => {
-  const elClass = e.target.classList;
-  if (!elClass.contains('active')) {
-    items.forEach(item => {
-      item.classList.remove('active');
-    });
-    elClass.add('active');
-  }
+// ratingbox.addEventListener('click', (e) => {
+//   const elClass = e.target.classList;
+//   if (!elClass.contains('active')) {
+//     items.forEach(item => {
+//       item.classList.remove('active');
+//     });
+//     elClass.add('active');
+//   }
 
-});
+// });
 
 
 
